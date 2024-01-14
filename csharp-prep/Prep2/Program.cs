e3a202b70a3dca9536d9cef5e1dc5a01f0820ea5
issahakuffo <issahakuffo300@gmail.com>
2024-01-14 01:58:14 +0000

    adding grades

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,47 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your score percentage? ");
+        string percentage = Console.ReadLine();
+        int grade = int.Parse(percentage);
+
+        string letter = "";
+
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+
+        else
+        {
+            letter = "Fail";
+        }
+
+        Console.WriteLine($"Your your grade is {letter}");
+
+
+        if (grade >= 70)
+        {
+            Console.WriteLine($"Congratulation! You Passes!");
+        }
+        else
+        {
+            Console.WriteLine($"More room for improvement!");
+        }
     }
 }
